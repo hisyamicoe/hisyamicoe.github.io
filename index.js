83feb1bd29aa97d5f3e4f5e7518164078fbf4fa7
hisyamicoe <30947441+hisyamicoe@users.noreply.github.com>
2017-08-12 14:20:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
 
       $("#surprise-button").click(function(e) {
-        $.get("https://media.giphy.com/media/CyNwabts0egVy/giphy.gif", function(res) {
+        $.get("http://api.giphy.com/v1/gifs/random?tag=cat&api_key=dc6zaTOxFJmzC", function(res) {
           $('#haha').html('<img src="' + res.data.image_url + '">' )
         })
       })
